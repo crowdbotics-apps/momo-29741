e66115668d0b6ca8c32f93bd513b001498f58ab7
Crowdbotics <team@crowdbotics.com>
2021-08-12 01:25:43 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "momo_29741Identifier",
+  url: "https://momo_29741Identifier.botics.co",
+  api: "https://momo_29741Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
